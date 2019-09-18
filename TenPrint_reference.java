7193bf77f205afa5822fd29cfba49abb0dd745db
Jason Cahill <9680033+jcahillmsft@users.noreply.github.com>
2019-09-17 22:45:14 -0700

    Initial commit
    Content for Sept 18, 2019 class at Sky Valley Education Center, Monroe, WA

---
+++ TenPrint_reference.java
@@ -0,0 +1,8 @@
+
+public class TenPrint {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.print((char)(9585 + Math.random() + 0.5));
+        }
+    }
+}
